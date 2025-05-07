482021385ff332df832109f62ab221b002bb7b9d
Entisar <Code.g.light@gmail.com>
2025-05-07 13:41:35 +0300

    ...

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const person = [{
-    name: "Intisar",
+    name: "Noura",
     age: 33,
+    gender: 'female',
     img: "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1315.jpg?w=826"
 }, {
     name: "Abdulaziz",
     age: 25,
+    gender : 'male',
     img: "https://img.freepik.com/premium-vector/avatar-profile-picture-icon-blue-background-flat-design-style-resources-graphic-element-design_991720-653.jpg?ga=GA1.1.660481976.1745316751&semt=ais_hybrid&w=740"
 }, {
     name: "Alice",
     age: 28,
+    gender: "female",
     img: "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1328.jpg?ga=GA1.1.660481976.1745316751&semt=ais_hybrid&w=740"
 }, {
     name: "John",
     age: 35,
+    gender : 'male',
     img: "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1339.jpg?ga=GA1.1.660481976.1745316751&semt=ais_hybrid&w=740"
 }];
 
@@ -23,7 +27,7 @@ for (let i = 0; i < person.length; i++) {
     img.width = 100;
     img.src = person[i].img;
     let name = document.createElement("p");
-    name.textContent = `The user name is ${person[i].name}`;    
+    name.textContent = `The user name is ${person[i].name} and their age is ${person[i].age}`;        
     document.body.appendChild(img);
     document.body.appendChild(name);
  }
